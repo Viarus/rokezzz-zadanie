6ef83a75b1601640642cef2edb542a3570502057
Viarus <videomontagefactory@gmail.com>
2025-11-14 17:16:47 +0100

    added main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    title: 'Andrzej Musłkuł - Twój wąsaty trener personalny',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
