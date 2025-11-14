744fdfad43eb3f3cb64c94387d8b5bfca5fc0880
Viarus <videomontagefactory@gmail.com>
2025-11-14 19:14:27 +0100

    working on animations

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,17 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  private router = inject(Router);
+  protected isOpen = false;
+
+  constructor() {
+    toObservable(this.router.currentNavigation)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => (this.isOpen = false));
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}
