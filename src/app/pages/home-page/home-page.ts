fd3a4a17b632658b7a391f4e981868c768d7b265
Viarus <videomontagefactory@gmail.com>
2025-11-15 20:46:40 +0100

    fixing button

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PhotoWithDescription } from '../../shared/photo-with-description/photo-with-description';
 import { GetStartedButton } from '../../shared/get-started-button/get-started-button';
+import { ContactForm } from '../../shared/contact-form/contact-form';
 
 @Component({
   selector: 'app-home-page',
-  imports: [PhotoWithDescription, GetStartedButton],
+  imports: [PhotoWithDescription, GetStartedButton, ContactForm],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
--- src/app/shared/get-started-button/get-started-button.ts
+++ src/app/shared/get-started-button/get-started-button.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-get-started-button',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './get-started-button.html',
   styleUrl: './get-started-button.scss',
 })
