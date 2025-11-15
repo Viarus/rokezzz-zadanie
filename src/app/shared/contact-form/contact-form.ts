b37ded6ce236e4542e0b7e8937ac51b7319360df
Viarus <videomontagefactory@gmail.com>
2025-11-15 21:23:29 +0100

    working on the form

--- src/app/shared/contact-form/contact-form.ts
+++ src/app/shared/contact-form/contact-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.minLength(6)]),
+  });
 
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
 }
