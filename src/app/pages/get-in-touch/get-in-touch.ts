353c9f0e1bf7c94cfdd7e9bb62ff8e0797bf753e
Viarus <videomontagefactory@gmail.com>
2025-11-16 21:10:06 +0100

    improving some pages

--- src/app/pages/get-in-touch/get-in-touch.ts
+++ src/app/pages/get-in-touch/get-in-touch.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactForm } from '../../shared/contact-form/contact-form';
 
 @Component({
   selector: 'app-get-in-touch',
-  imports: [],
+  imports: [ContactForm],
   templateUrl: './get-in-touch.html',
   styleUrl: './get-in-touch.scss',
 })
-export class GetInTouch {
-
-}
+export class GetInTouch {}
