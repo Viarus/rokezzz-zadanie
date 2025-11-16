813c2413b11a4bf23f64149a76e43a9316e9e666
Viarus <videomontagefactory@gmail.com>
2025-11-16 21:06:13 +0100

    working on transformations page

--- src/app/pages/transformations/transformations.ts
+++ src/app/pages/transformations/transformations.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-transformations',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './transformations.html',
   styleUrl: './transformations.scss',
 })
-export class Transformations {
-
-}
+export class Transformations {}
