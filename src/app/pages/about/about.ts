245d70b0e7de440d344d66fa48c83578e01df7b9
Viarus <videomontagefactory@gmail.com>
2025-11-16 19:05:17 +0100

    working on about page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PhotoWithDescription } from '../../shared/photo-with-description/photo-with-description';
+import { GetStartedButton } from '../../shared/get-started-button/get-started-button';
 
 @Component({
   selector: 'app-about',
-  imports: [PhotoWithDescription],
+  imports: [PhotoWithDescription, GetStartedButton],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
