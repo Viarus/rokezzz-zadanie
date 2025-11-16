a59518aca18bdc2af3e5626385fc97c3d1dc3d3a
Viarus <videomontagefactory@gmail.com>
2025-11-16 17:29:47 +0100

    adding rest of the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,35 @@ export const routes: Routes = [
     title: 'Andrzej Musłkuł - Twój wąsaty trener personalny',
   },
   {
+    path: 'achievements',
+    redirectTo: 'about',
+  },
+  {
+    path: 'transformations',
+    loadComponent: () =>
+      import('./pages/transformations/transformations').then((m) => m.Transformations),
+    title: 'Andrzej Musłkuł - Przemiany',
+  },
+  {
+    path: 'gallery',
+    loadComponent: () => import('./pages/gallery/gallery').then((m) => m.Gallery),
+    title: 'Andrzej Musłkuł - Galeria',
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+    title: 'Andrzej Musłkuł - O mnie',
+  },
+  {
+    path: 'training',
+    redirectTo: 'get-in-touch',
+  },
+  {
+    path: 'get-in-touch',
+    loadComponent: () => import('./pages/get-in-touch/get-in-touch').then((m) => m.GetInTouch),
+    title: 'Andrzej Musłkuł - Kontakt',
+  },
+  {
     path: '**',
     redirectTo: '',
   },
